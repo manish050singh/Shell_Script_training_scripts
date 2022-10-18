438b1b8eb750732bbac4da8254a31d0938f06710
toarnabtrainer <toarnabtrainer@gmail.com>
2022-10-18 10:42:09 +0530

    Commit-9

---
+++ shell17.sh
@@ -0,0 +1,38 @@
+# Pattern printing - 2 (Class Assignment - 1 / Day-2)
+# 
+# n = 6 (user given number)
+#                i    .    *
+#             -----------------
+# ***********    1    0   11       (n, i)
+# .*********     2    1    9  . => (????)
+# ..*******      3    2    7
+# ...*****       4    3    5  * => (????)
+# ....***        5    4    3
+# .....*         6    5    1
+#             -----------------
+#             Tracing Table (For finding logic)
+# 
+
+echo -n "Please enter the number of layers: "
+read n
+i=1
+while [ $i -le $n ] ; do
+	blank=`expr $n - $i`
+	j=1
+	while [ $j -le $blank ] ; do
+		echo -n "."
+		j=`expr $j + 1`
+	done
+
+	star=`expr 2 \* $i - 1`
+	j=1
+	while [ $j -le $star ] ; do
+		echo -n "*"
+		j=`expr $j + 1`
+	done
+	echo ""
+	i=`expr $i + 1`
+done
+
+echo -e "\nEnd of the shell script...\n"
+
