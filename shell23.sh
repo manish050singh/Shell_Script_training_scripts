777c17e361343729413983e0f33a587a9be9f696
toarnabtrainer <toarnabtrainer@gmail.com>
2022-10-18 13:24:34 +0530

    Commit-12

---
+++ shell23.sh
@@ -0,0 +1,10 @@
+# Calendar shell script with roman month number as command line argument (Class Assignment - 2 / Day-2)
+
+# Write a shell script to modify the cal command of Linux/Unix so that it prints the calendar of the corresponding month against the given year when the month number is given in roman. E.g. I, II, III, IV, ..., XII. Where I is for April, II is for May and in this way XI is for February and XII is for March.
+
+# So as a result 'shell23.sh X 2020' will generate January 2020 calendar as output.
+# So 'shell23.sh X 2020' is equivalent to 'cal 1 2020'
+
+
+
+
